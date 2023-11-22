28aec6b0de04d31397e47fc95e1074da5e8877ab
Anonymous <87971370+anonymous-1001@users.noreply.github.com>
2023-11-22 16:17:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,108 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+
+import mysql.connector
 
 app = Flask(__name__)
 
-# Dummy admin credentials (in a real app, use a more secure method for storing credentials)
-admin_password = 'password'
+app.secret_key = 'xyz'
 
-# Dummy posts data
-posts = [
-    {'title': 'Blog Post Title', 'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis turpis eget enim lacinia, vel vestibulum elit laoreet.'},
-    {'title': 'Another Blog Post Title', 'content': 'Phasellus congue libero at magna imperdiet, vitae consectetur libero rutrum.'}
-]
+# Configure MySQL connection
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='flask'
+)
+
+cursor = db.cursor(buffered=True)
 
 @app.route('/')
 def index():
-    return render_template('index.html', posts=posts)
+    if 'username' in session:
+        cursor = db.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM posts')  # Replace 'posts' with your actual table name
+        posts = cursor.fetchall()
+        cursor.close()
+        username = session['username']
+        return render_template('index.html', username=username, posts=posts)
+    else:
+        return redirect(url_for('login'))
 
-@app.route('/admin', methods=['GET', 'POST'])
-def admin():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
+        username = request.form['username']
         password = request.form['password']
-        if password == admin_password:
-            return render_template('admin.html')
+        
+        # Execute a SELECT query to check user credentials
+        query = "SELECT * FROM users WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
+        user = cursor.fetchone()
+        
+        if user:
+            # User authenticated
+            # Perform actions after successful login (e.g., redirect to dashboard)
+            session['logged_in'] = True
+            session['username'] = username
+            return redirect(url_for('dashboard'))
         else:
-            return render_template('login.html', message='Invalid password. Please try again.')
+            return render_template('login.html', message='Invalid username or password. Please try again.')
     else:
         return render_template('login.html', message='')
 
-@app.route('/add_post', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Check if the username already exists
+        check_query = "SELECT * FROM users WHERE username = %s"
+        cursor.execute(check_query, (username,))
+        existing_user = cursor.fetchone()
+        
+        if existing_user:
+            return render_template('register.html', message='Username already exists. Please choose a different one.')
+        else:
+            # Insert new user data into the database
+            insert_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+            cursor.execute(insert_query, (username, password))
+            db.commit()  # Commit changes to the database
+            
+            return redirect(url_for('login'))
+    else:
+        return render_template('register.html', message='')
+    
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        username = session['username']
+        return render_template('dashboard.html', username=username)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/add_post', methods=['GET', 'POST'])
 def add_post():
     if request.method == 'POST':
-        try:
-            title = request.form['title']
-            content = request.form['content']
-            new_post = {'title': title, 'content': content}
-            posts.append(new_post)
-            print("Post added successfully:", new_post)  # Print the newly added post
-        except Exception as e:
-            print("Error adding post:", e)  # Print any exception/error that occurs while adding the post
-    return redirect(url_for('index'))
+        title = request.form['title']
+        content = request.form['content']
+        
+        # Insert the post into the database
+        insert_query = "INSERT INTO posts (title, content) VALUES (%s, %s)"
+        cursor.execute(insert_query, (title, content))
+        db.commit()  # Commit the changes
+        
+        return redirect(url_for('dashboard'))
+    return render_template('add_post.html')
 
+@app.route('/logout')
+def logout():
+    # Remove user session data
+    session.pop('logged_in', None)
+    session.pop('username', None)
+    
+    # Redirect to home page after logout
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
