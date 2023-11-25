ba793509fb5f8752e3d68857e3da92e8ce2988d6
chuup <97285423+chuup@users.noreply.github.com>
2023-11-25 08:29:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -215,5 +215,7 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
     
-    app.run(debug=True)
+    #app.run(debug=True)
